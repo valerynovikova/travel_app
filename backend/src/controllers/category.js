10886ecfe635d8f35ef19ea4c11d7aeccdfe315c
modylegi <modylegi@gmail.com>
2025-05-03 13:36:18 +0300

    fix category

--- backend/src/controllers/category.js
+++ backend/src/controllers/category.js
@@ -2,79 +2,77 @@ const Category = require('../models/category');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
-exports.categoryById = (req, res, next, id) => {
-    Category.findById(id).exec((err, category) => {
-        if (err || !category) {
+exports.categoryById = async (req, res, next, id) => {
+    try {
+        const category = await Category.findById(id);
+        if (!category) {
             return res.status(400).json({
                 error: 'Category does not exist'
             });
         }
         req.category = category;
         next();
-    });
+    } catch (err) {
+        return res.status(400).json({
+            error: 'Error fetching category'
+        });
+    }
 };
 
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
     const category = new Category(req.body);
-    category.save((err, data) => {
-        if (err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
+    try {
+        const data = await category.save();
         res.json({ data });
-    });
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler(err)
+        });
+    }
 };
 
 exports.read = (req, res) => {
     return res.json(req.category);
 };
 
-exports.update = (req, res) => {
-    console.log('req.body', req.body);
-    console.log('category update param', req.params.categoryId);
-
+exports.update = async (req, res) => {
     const category = req.category;
     category.name = req.body.name;
-    category.save((err, data) => {
-        if (err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
+    try {
+        const data = await category.save();
         res.json(data);
-    });
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler(err)
+        });
+    }
 };
 
-exports.remove = (req, res) => {
+exports.remove = async (req, res) => {
     const category = req.category;
-    Product.find({ category }).exec((err, data) => {
-        if (data.length >= 1) {
+    try {
+        const products = await Product.find({ category });
+        if (products.length >= 1) {
             return res.status(400).json({
-                message: `Sorry. You cant delete ${category.name}. It has ${data.length} associated products.`
-            });
-        } else {
-            category.remove((err, data) => {
-                if (err) {
-                    return res.status(400).json({
-                        error: errorHandler(err)
-                    });
-                }
-                res.json({
-                    message: 'Category deleted'
-                });
+                message: `Sorry. You can't delete ${category.name}. It has ${products.length} associated products.`
             });
         }
-    });
+        await category.deleteOne();
+        res.json({ message: 'Category deleted' });
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler(err)
+        });
+    }
 };
 
-exports.list = (req, res) => {
-    Category.find().exec((err, data) => {
-        if (err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
+exports.list = async (req, res) => {
+    try {
+        const data = await Category.find();
         res.json(data);
-    });
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler(err)
+        });
+    }
 };
